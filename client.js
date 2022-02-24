a611ca44565777cefb31b9737ae69aab812e656e
Simon Boyd <sboyd@companieshouse.gov.uk>
2022-02-24 09:42:40 +0000

    project module 3

--- client.js
+++ client.js
@@ -1,46 +1,81 @@
-const express = require("express")
-const bodyParser = require("body-parser")
-const axios = require("axios").default
-const { randomString, timeout } = require("./utils")
+const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios').default;
+const { randomString, timeout } = require('./utils');
 
 const config = {
-	port: 9000,
+  port: 9000,
 
-	clientId: "my-client",
-	clientSecret: "zETqHgl0d7ThysUqPnaFuLOmG1E=",
-	redirectUri: "http://localhost:9000/callback",
+  clientId: 'my-client',
+  clientSecret: 'zETqHgl0d7ThysUqPnaFuLOmG1E=',
+  redirectUri: 'http://localhost:9000/callback',
 
-	authorizationEndpoint: "http://localhost:9001/authorize",
-	tokenEndpoint: "http://localhost:9001/token",
-	userInfoEndpoint: "http://localhost:9002/user-info",
-}
-let state = ""
+  authorizationEndpoint: 'http://localhost:9001/authorize',
+  tokenEndpoint: 'http://localhost:9001/token',
+  userInfoEndpoint: 'http://localhost:9002/user-info'
+};
+let state = '';
 
-const app = express()
-app.set("view engine", "ejs")
-app.set("views", "assets/client")
-app.use(timeout)
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+const app = express();
+app.set('view engine', 'ejs');
+app.set('views', 'assets/client');
+app.use(timeout);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 /*
 Your code here
 */
+app.get('/authorize', (req, res) => {
+  state = randomString();
+  const authUrl = new URL(config.authorizationEndpoint);
+  authUrl.searchParams.append('response_type', 'code');
+  authUrl.searchParams.append('client_id', config.clientId);
+  authUrl.searchParams.append('redirect_uri', config.redirectUri);
+  authUrl.searchParams.append('scope', 'permission:name permission:date_of_birth');
+  authUrl.searchParams.append('state', state);
 
-const server = app.listen(config.port, "localhost", function () {
-	var host = server.address().address
-	var port = server.address().port
-})
+  return res.redirect(authUrl);
+});
+
+app.get('/callback', (req, res) => {
+  const { code: qcode, state: qstate } = req.query;
+  if (state !== qstate) {
+    return res.status(403).end();
+  }
+  else {
+    axios({
+      method: 'POST',
+      url: config.tokenEndpoint,
+      auth: { username: config.clientId, password: config.clientSecret },
+      data: { code: qcode }
+    }).then((postResponse) => {
+      const { access_token } = postResponse.data;
+      return axios({
+        method: 'GET',
+        url: config.userInfoEndpoint,
+        headers: { authorization: `Bearer ${access_token}` }
+      }).then((getResponse) => {
+        res.render('welcome', { user: getResponse.data });
+      });
+    });
+  }
+});
+
+const server = app.listen(config.port, 'localhost', function() {
+  var host = server.address().address;
+  var port = server.address().port;
+});
 
 // for testing purposes
 
 module.exports = {
-	app,
-	server,
-	getState() {
-		return state
-	},
-	setState(s) {
-		state = s
-	},
-}
+  app,
+  server,
+  getState() {
+    return state;
+  },
+  setState(s) {
+    state = s;
+  }
+};
